dce8edf6eb2774672d400660aa0bbe79d00b162d
Daniel Thomas <tfriesdan@gmail.com>
2019-04-02 11:43:45 -0700

    started Lab 1

---
+++ Daily/Arrays/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sum;
+const int ARRAY_SIZE = 7;
+int grid [10][10];
+
+int main()
+{
+    for (int row = 0; row < 10; row++)
+    {
+       for (int col = 0; col < 10; col++)
+       {
+           cout << grid
+       }
+    }
+
+    return 0;
+}
+
---
+++ Lab 1/DanielFriesLab1/main.cpp
@@ -0,0 +1,33 @@
+//  Daniel Fries
+//  CS162+
+//  Lab 1
+//  Tic Tac Toe
+
+
+#include <iostream>
+
+using namespace std;
+
+char board[3][3];
+
+void displayInstructions();
+void showBoard(char[][3]);
+int getMove();
+bool checkWin();
+bool checkDraw();
+
+int main()
+{
+
+    showBoard(board);
+
+    return 0;
+}
+void displayInstructions()
+{
+
+}
+void showBoard(char);
+int getMove();
+bool checkWin();
+bool checkDraw();
